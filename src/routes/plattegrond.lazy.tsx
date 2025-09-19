0a2f0d016fa48277db9e8d758dfc0ae3ed440580
AZ Software <66299945+AZProductions@users.noreply.github.com>
2025-09-19 17:01:13 +0200

    Add SidePanel to plattegrond route

--- src/routes/plattegrond.lazy.tsx
+++ src/routes/plattegrond.lazy.tsx
@@ -1,9 +1,35 @@
+import { SidePanel } from "@carbon/ibm-products";
+import { Button, Tile } from "@carbon/react";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 
 export const Route = createLazyFileRoute("/plattegrond")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/plattegrond"!</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Tile>
+        <Button onClick={() => setOpen((prev) => !prev)}>
+          Open side panel
+        </Button>
+      </Tile>
+      <SidePanel
+        size="sm"
+        open={open}
+        slideIn={false}
+        animateTitle={false}
+        includeOverlay={false}
+        onRequestClose={() => setOpen(false)}
+        selectorPageContent="#main-content"
+        title="Title"
+        placement="right"
+        subtitle="Testing subtitle text."
+      >
+        <div id="main-content">Lorem ipsum dolor sit amet...</div>
+      </SidePanel>
+    </>
+  );
 }
